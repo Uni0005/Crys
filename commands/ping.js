a9fb43a1f0c3519cf5b4e69141d7dafa385be429
Kremix <holandsol6@gmail.com>
2022-04-17 19:55:14 +0300

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Just test'),
 	async execute(interaction) {
 		return interaction.reply('Pong!');
 	},
