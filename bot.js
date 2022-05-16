87a9b40d13613e5c0e2909efbb13708fbda0ca0a
EXPress-016 <skatif95939@gmail.com>
2022-05-16 15:02:39 +0530

    better event handler

--- bot.js
+++ bot.js
@@ -1,21 +1,25 @@
-const { token, mongo_uri, token2} = require('./cfg.json');
+const { token, mongo_uri, token2 } = require('./cfg.json');
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [581] });
 const fs = require('node:fs');
 const mongoose = require('mongoose');
 
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
 
-require('./events/ready')(client);
-require('./events/lvl-system')(client);
-require('./events/lvl-roles')(client);
-require('./events/star')(client);
-require('./events/ycc')(client);
+const eventFiles = fs
+  .readdirSync('./events')
+  .filter((file) => file.endsWith('.js'));
+
+for (const event of eventFiles) {
+  require(`./events/${event}`)(client);
+}
 
 client.login(token);
